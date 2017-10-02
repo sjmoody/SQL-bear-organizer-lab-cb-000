f04de61ca9f707478939f2c5baa65c24fb3582f9
sjmoody <steven@beachheadmarketing.com>
2017-10-02 16:56:02 +0200

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN,
+  id INTEGER PRIMARY KEY
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ("Mr. Chocolate", 3, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 1, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 1, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown", 1, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 1, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Grinch", 1, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 1, "F");
+INSERT INTO bears (name, age, gender) VALUES (NULL, 1, "F");
